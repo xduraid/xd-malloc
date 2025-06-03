f2b41a04bcb95016b0c00b87f9f39ff1a8667188
Duraid Maihoub <duraidmaihoub@gmail.com>
2025-06-02 20:26:38 -0700

    first commit

---
+++ include/xd_malloc.h
@@ -0,0 +1,20 @@
+/*
+ * ==============================================================================
+ * File: xd_malloc.h
+ * Author: Duraid Maihoub
+ * Date: 1 June 2025
+ * Description: Part of the xd-malloc project.
+ * Repository: https://github.com/xduraid/xd-malloc
+ * ==============================================================================
+ * Copyright (c) 2025 Duraid Maihoub
+ *
+ * xd-malloc is distributed under the MIT License. See the LICENSE file
+ * for more information.
+ * ==============================================================================
+ */
+
+#ifndef XD_MALLOC_H
+#define XD_MALLOC_H
+
+int foo();
+#endif  // XD_MALLOC_H
